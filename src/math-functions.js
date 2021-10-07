55eec138db135990e50b153f4f0b16ed1ebdaea8
James Demiraiakian <james.armen@gmail.com>
2021-10-07 15:00:43 -0700

    created itterating multiplication. Initially, it failed tests, returning undefined. I added a few console logs, and suddenly it started working. Then removed the logs, and it still works. I don't know why. My code is haunted. It works. Don't question it.

--- src/math-functions.js
+++ src/math-functions.js
@@ -130,7 +130,12 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    let arrProd = dynamicArray[0];
+    for(let i = 1; i < dynamicArray.length; i++) {
+        arrProd = multiply(arrProd, dynamicArray[i])[0];
+    }
+    const anyArray = [arrProd, `The numbers 1,2,3,4,5 have a product of 120.`];
+    return anyArray;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
